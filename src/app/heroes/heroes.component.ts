6c6b4cff7dabd51c81b0b32f147763076f0b7f91
Kinrany <kinrany@yandex.com>
2017-12-17 21:19:35 +0300

    added a name property to HeroesComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    this.name = 'Windstorm';
+  }
 
   ngOnInit() {
   }
+
+  name: string;
 
 }
