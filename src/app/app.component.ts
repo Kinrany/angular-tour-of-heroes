e0fcc25cfadba9008c26219dd864c8e50b4b1312
Kinrany <kinrany@yandex.com>
2018-01-13 19:03:13 +0300

    inlined app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .italic {
+      font-style: italic
+    }
+  `],
+  template: `
+    <h1 class="italic"> {{title}} </h1>
+    <app-heroes></app-heroes>
+  `
 })
 export class AppComponent {
   title = 'Tour of Heroes';
