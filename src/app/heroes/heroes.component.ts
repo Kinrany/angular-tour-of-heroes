b317f6f7b274d8e0394648ccf28fb7dd632ddffb
Kinrany <kinrany@yandex.com>
2018-01-13 20:03:51 +0300

    hero choises generate messages too

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 
 import { HeroService } from '../hero.service';
+import { MessagesService } from '../messages.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,7 +11,7 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messagesService: MessagesService) { }
 
   ngOnInit() {
     this.getHeroes();
@@ -22,6 +23,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messagesService.add(`HeroesComponent: ${hero.name} selected.`);
   }
 
   getHeroes(): void {
